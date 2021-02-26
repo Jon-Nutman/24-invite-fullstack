8708c58c9b18abae88eead7634ad68bd2674c1e6
Jon-Nutman <jonnut730@gmail.com>
2021-02-26 18:03:57 -0500

    profile card basics

--- client/src/features/invite/InvitePage.js
+++ client/src/features/invite/InvitePage.js
@@ -1,15 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 export default function InvitePage() {
     // refactor to use slice
+const[person, setPerson] = useState({})
+
     useEffect(() => {
-        axios.get('/testing')
+        axios.get('https://randomuser.me/api/')
         .then(resp => {
-            console.log(resp)
+            console.log(resp.data.results[0])
+        setPerson(resp.data.results[0])
         })
     }, [])
+
     return (
-        <div>invite</div>
+       
+    <div>
+     <span><img src={person.picture ? person.picture.medium : 'n/a'} alt='profile pic'/></span>
+     <span>{person.name ? person.name.first : 'n/a'} {person.name ? person.name.last : 'n/a'}</span>
+     <span>{person.cell}</span>
+     <span>{person.email}</span>
+    </div>
     )
 }
